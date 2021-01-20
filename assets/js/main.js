433e923c49d63779c61c6062e52990b74e3821da
angelacaterina <angela.caterina@icloud.com>
2021-01-20 17:08:25 +0100

    sezione culinary_collection
    finita

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,41 @@ let app = new Vue({
       title: 'Food Corner: Top Japanese',
       subTitle:'Restaurants for Sushi',
       day: 'March 25,2019',
-    }
+    },
+    icons:[
+      {
+        name:'DRINKS',
+        url: 'assets/images/icone/drinks-recipes.png',
+      },
+      {
+        name:'SOUP',
+        url: 'assets/images/icone/soups-recipes.png',
+      },
+      {
+        name:'BAKERY',
+        url: 'assets/images/icone/baking-recipes.png',
+      },
+      {
+        name:'DINNER',
+        url: 'assets/images/icone/dinner-recipes.png',
+      },
+      {
+        name:'HEALTHY',
+        url: 'assets/images/icone/healthy-recipes.png',
+      },
+      {
+        name:'STAFF PICKS',
+        url: 'assets/images/icone/staff-picks.png',
+      },
+      {
+        name:'APPETISERS',
+        url: 'assets/images/icone/premium-recipes.png',
+      },
+      {
+        name:'QUICK & EASY',
+        url: 'assets/images/icone/quick-easy-recipes.png',
+      }
+    ]
 
   }
 })
