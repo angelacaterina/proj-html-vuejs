2b970fc62a39fa9f097de8bbc24aa758b67576e3
angelacaterina <angela.caterina@icloud.com>
2021-01-21 01:05:48 +0100

    main sezione #images comletata
    revisione del css, html e js

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,8 +48,7 @@ let app = new Vue({
       }
     ],
     banner:{
-      title: 'Food Corner: Top Japanese',
-      subTitle:'Restaurants for Sushi',
+      title: 'Food Corner: Top Japanese Restaurants for Sushi',
       day: 'March 25,2019',
     },
     icons:[
@@ -85,7 +84,71 @@ let app = new Vue({
         name:'QUICK & EASY',
         url: 'assets/images/icone/quick-easy-recipes.png',
       }
-    ]
+    ],
+    popularRecipes:[
+      {
+        icon: 'fas fa-link',
+        title:'Fruit Platter with Banana, Mango, Berries and Orange',
+        url: 'assets/images/Mixed-fruits.jpg',
+      },
+      {
+        icon: 'fas fa-link',
+        title:'Breakfast Delight With Strawberry, Egg And Fruit',
+        url: 'assets/images/r-rachel-park-366508-unsplash-min.jpg',
+      },
+      {
+        icon: 'fas fa-link',
+        title:'Ice Cream Heaven With Vanilla, Chocolate And Pistachio',
+        url: 'assets/images/r-michelle-tsang-500721-unsplash-min.jpg',
+      },
+      {
+        icon: 'fas fa-link',
+        title:'Video Recipe: How to Make a Cool Summer Drink',
+        url: 'assets/images/quick-summer-drink-460x295.jpg',
+      },
+      {
+        icon: 'fas fa-link',
+        title:'Ketogenic Diet Recipe with Avocado',
+        url: 'assets/images/r-maarten-van-den-heuvel-400626-unsplash-min-460x295.jpg',
+      },
+      {
+        icon: 'fas fa-link',
+        title:'Video Recipe: The Perfect Cosmopolitan',
+        url: 'assets/images/perfect-cosmopolitan-460x295.jpg',
+      },
+      {
+        icon: 'fas fa-link',
+        title:'Summer Cheese Platter with Berrie, Crackers and Wine',
+        url: 'assets/images/fi2x-6-460x295.jpg',
+      },
+      {
+        icon: 'fas fa-link',
+        title:'Strawberry Fruit Pies Served With Tea',
+        url: 'assets/images/r-brooke-lark-96398-unsplash-min-460x295.jpg',
+      },
+    ],
+    image:[
+      {
+        icon: 'fas fa-link',
+        title:'Lunch Favourite with Salad, Naan And Beans',
+        url: 'assets/images/Yogurt-Nan.jpg',
+      },
+      {
+        icon: 'fas fa-link',
+        title:'Fruit Platter with Banana, Mango, Berries and Orange',
+        url: 'assets/images/Mixed-fruits.jpg',
+      },
+      {
+        icon: 'fas fa-link',
+        title:'Breakfast Delight With Strawberry, Egg And Fruit',
+        url: 'assets/images/r-rachel-park-366508-unsplash-min.jpg',
+      },
+      {
+        icon: 'fas fa-link',
+        title:'Ice Cream Heaven With Vanilla, Chocolate And Pistachio',
+        url: 'assets/images/r-michelle-tsang-500721-unsplash-min.jpg',
+      },
+    ],
 
   }
 })
